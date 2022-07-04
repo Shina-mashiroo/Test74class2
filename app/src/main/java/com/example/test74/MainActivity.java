d2dc33130baad3f0718bc8a59a678ba6e4fea1e5
Shina <184806661lv@gmail.com>
2022-07-04 15:49:16 +0800

    Test74(2)

--- app/src/main/java/com/example/test74/MainActivity.java
+++ app/src/main/java/com/example/test74/MainActivity.java
@@ -3,49 +3,52 @@ package com.example.test74;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-
+    List<String> stringList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        listView = findViewById(R.id.lv);
-//        List<String> stringList = new ArrayList<>();
-//        for (int i = 0 ; i < 30 ; i++) {
-//            stringList.add("item"+i);
-//        }
-//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this,R.layout.item,stringList);
-//        listView.setAdapter(arrayAdapter);
-//        listView.setSelection(10);
-//        ImageView imageView = new ImageView(this);
-//        imageView.setImageResource(R.drawable.ic_launcher_background);
-//        listView.addHeaderView(imageView);
-//        listView.addFooterView(imageView);
-        listView=findViewById(R.id.lv);
-        List<Map<String,Object>> mapList = new ArrayList<>();
-        int[] images=new int[]{R.drawable.classshop,R.drawable.buyshop,R.drawable.seeshop,R.drawable.mineshop,R.drawable.seeshop};
-        String []names = new String[]{"首页","分类","购物","逛逛","购物"};
-        for (int i = 0 ; i<names.length ; i++) {
-            Map<String,Object> stringObjectMap = new HashMap<>();
-            stringObjectMap.put("name",names[i]);
-            stringObjectMap.put("image",images[i]);
-            mapList.add(stringObjectMap);
+        listView = findViewById(R.id.lv);
+        stringList = new ArrayList<>();
+        for (int i = 0; i<30 ; i++) {
+            stringList.add("item"+i);
         }
-        SimpleAdapter simpleAdapter = new SimpleAdapter(MainActivity.this,
-                mapList,
-                R.layout.simitem,
-                new String[]{"name","image"},
-                new int[]{R.id.tv,R.id.iv});
-
-        listView.setAdapter(simpleAdapter);
+        MybaseAdapterArr myBaseAdapterarr = new MybaseAdapterArr();
+        listView.setAdapter(myBaseAdapterarr);
     }
+    public class  MybaseAdapterArr extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return stringList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView = new TextView(MainActivity.this);
+            textView.setText(stringList.get(position));
+            return textView;
+        }
+    }
+
 }
